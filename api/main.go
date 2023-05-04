5b99ffd1bda5db32404101af858813070bb008c2
schuck <zzztttztzt@126.com>
2023-05-04 14:21:50 +0800

    add some comment

--- api/main.go
+++ api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
@@ -18,32 +17,30 @@ import (
 const configFile = "../config/dev.yaml"
 
 func main() {
-	testDataStructure()
-	r := gin.New()
 
+	// initialize gin router
+	r := gin.New()
 	r.Use(middleware.DefaultStructedLogger())
 	r.Use(gin.Recovery())
 
+	// load config
 	cfg := loadConfig(configFile)
-
 	log.Info().Msg(fmt.Sprintf("config is %v", cfg))
-	ctx.NewAppContext(cfg)
 
+	// initialize app context
+	ctx.NewAppContext(cfg)
 	defer ctx.AppCtx.DestroyAppCtx()
 
+	// define router
 	r.GET("/chat", lib.GetRouter)
 
-	log.Print("prepare to listen on 8888")
-
+	// set up logger
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
-
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	gin.DefaultWriter = log.Logger
 
-	err := errors.New("mock err here")
-	log.Error().Stack().Err(err).Msg("a info log")
-	err = r.Run(":8888")
+	err := r.Run(":8888")
 	if err != nil {
 		return
 	}
@@ -69,13 +66,4 @@ func loadConfig(path string) config.Config {
 	}
 
 	return cfg
-}
-
-func testDataStructure() {
-	//myStack := datastructure.New[int32]()
-	//
-	//fmt.Println(myStack.Pop())
-	//myStack.Push(44)
-	//fmt.Println(myStack.Pop())
-
 }
